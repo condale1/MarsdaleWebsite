8952e62d6968de4ed860f1615f44238a0007dedd
ConnorDale <condale1@googlemail.com>
2023-08-15 17:57:12 +0100

    Loading page added, hero image centered

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const loader = document.getElementById("preloader");
+window.addEventListener("load", function(){
+  loader.style.display = "none";
+})
+
+
 let tl = gsap.timeline({ defaults: {duration: .5, opacity: 0}});
 tl.from(".hero__header", { y: '100%', ease: Power1.easeOut }, 1)
   .from(".hero__text", { y: '-100%', ease: Power1.easeOut }, '<.5')
