462105e4cc70dce672d374901bebbef60494e8c3
ConnorDale <condale1@googlemail.com>
2023-08-15 17:14:27 +0100

    Update gsap to timeline

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
-gsap.from(".hero__header", {
-  y: '100%',
-  opacity: 0,
-  duration: 1,
-  delay: 1,
-  ease: Power2.easeOut
-})
-
-gsap.from(".hero__text", {
-  y: '-100%',
-  opacity: 0,
-  duration: 1,
-  delay: 2,
-  ease: Power2.easeOut
-})
-
-gsap.from(".hero__btn", {
-  opacity: 0,
-  duration: 1,
-  delay: 3,
-  ease: Power2.easeOut
-});
+let tl = gsap.timeline({ defaults: {duration: .5, opacity: 0}});
+tl.from(".hero__header", { y: '100%', ease: Power1.easeOut }, 1)
+  .from(".hero__text", { y: '-100%', ease: Power1.easeOut }, '<.5')
+  .from(".hero__btn", { ease: Power1.easeOut}, '<.5')
