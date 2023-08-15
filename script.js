1d2e7106a66d420e1f4336cfde68f11eef55919a
ConnorDale <condale1@googlemail.com>
2023-08-15 18:35:55 +0100

    Add navbar hidding

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const loader = document.getElementById("preloader");
+const navbar = document.getElementsByClassName("navbar")[0];
+
 window.addEventListener("load", function(){
   loader.style.display = "none";
 })
+
+var lastScrollTop = 0;
+
+window.addEventListener("scroll", function() {
+  var st = window.pageYOffset || document.documentElement.scrollTop;
+  if (st > lastScrollTop) {
+   navbar.classList.add("hidden");
+  } else if (st < lastScrollTop) {
+     navbar.classList.remove("hidden");
+  }
+  lastScrollTop = st <= 0 ? 0 : st;
+}, false);
 
 
 let tl = gsap.timeline({ defaults: {duration: .5, opacity: 0}});
