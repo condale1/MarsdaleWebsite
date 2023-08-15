bf5029affaa8bac8bd8f8aaf684f4d29e7ab30f2
ConnorDale <condale1@googlemail.com>
2023-08-15 16:58:40 +0100

    about&footer change, responsive fix, animate

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-$(function(){
-    $("#nav_placeholder").load("nav.html");
-  });
+gsap.from(".hero__header", {
+  y: '100%',
+  opacity: 0,
+  duration: 1,
+  delay: 1,
+  ease: Power2.easeOut
+})
+
+gsap.from(".hero__text", {
+  y: '-100%',
+  opacity: 0,
+  duration: 1,
+  delay: 2,
+  ease: Power2.easeOut
+})
+
+gsap.from(".hero__btn", {
+  opacity: 0,
+  duration: 1,
+  delay: 3,
+  ease: Power2.easeOut
+});
