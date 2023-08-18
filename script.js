c3bff0757f418230387a8c6d1b89fb34bdadacb1
ConnorDale <condale1@googlemail.com>
2023-08-18 01:51:26 +0100

    New picture accordion

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const loader = document.getElementById("preloader");
 const navbar = document.getElementsByClassName("navbar")[0];
 const video = document.getElementById("VFX__video");
+const accordion = document.querySelector(".accordion");
 
 window.addEventListener("load", function () {
   loader.style.display = "none";
@@ -26,3 +27,30 @@ let tl = gsap.timeline({ defaults: { duration: 0.5, opacity: 0 } });
 tl.from(".hero__header", { y: "100%", ease: Power1.easeOut }, 1)
   .from(".hero__text", { y: "-100%", ease: Power1.easeOut }, "<.5")
   .from(".hero__btn", { ease: Power1.easeOut }, "<.5");
+
+
+  accordion.addEventListener("click", (e) => {
+    const activePanel = e.target.closest(".accordion-panel");
+    if (!activePanel) return;
+    toggleAccordion(activePanel);
+  });
+  
+  function toggleAccordion(panelToActivate) {
+    const buttons = panelToActivate.parentElement.querySelectorAll("button");
+    const contents =
+      panelToActivate.parentElement.querySelectorAll(".accordion-content");
+  
+    buttons.forEach((button) => {
+      button.setAttribute("aria-expanded", false);
+    });
+  
+    contents.forEach((content) => {
+      content.setAttribute("aria-hidden", true);
+    });
+  
+    panelToActivate.querySelector("button").setAttribute("aria-expanded", true);
+  
+    panelToActivate
+      .querySelector(".accordion-content")
+      .setAttribute("aria-hidden", false);
+  }
